ed6d7baac4714a4b12adf42149b5618e112fd738
dxtstd <noreply@github.com>
2024-02-11 21:50:08 +0800

    first commit :3

---
+++ assets/script/text.js
@@ -0,0 +1,43 @@
+(function () {
+    const text = [
+        'oyasumi.',
+        'everything will be okay :D',
+        'umm...',
+        'something behind you...'
+    ]
+    
+    let indexText = 0 
+    
+    const textAnimID = document.getElementById('textAnim')
+    const textAnimBefore = textAnimID.innerText
+    
+    const loopTextAnim = async function () {
+        const textAnim = text[indexText]
+        let textAnimLength = textAnim.length
+        
+        let textAnimAfter = ""
+        for await(const letter of textAnim.split('')) {
+        textAnimAfter += letter
+            await sleep(0.15)
+            document.getElementById('textAnim').innerText = `${textAnimBefore}${textAnimAfter}`
+            textAnimLength --
+        }
+        
+        await sleep(5)
+        
+        while (textAnimLength < (textAnim.length + 1)) {
+            textAnimLength ++
+            textAnimAfter = textAnimAfter.split('').reverse().splice(1).reverse().join('')
+            document.getElementById('textAnim').innerText = `${textAnimBefore}${textAnimAfter}`
+            await sleep(0.075)
+        }
+        
+        indexText ++
+        if (indexText > (text.length - 1)) {
+            indexText = 0
+        }
+        
+        loopTextAnim()
+    };
+    loopTextAnim()
+})()
